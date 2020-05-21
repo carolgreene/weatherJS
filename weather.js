39b45580b973ff5aaf071709fe556d024059f5d4
carolgreene <carolgreene3@gmail.com>
2020-05-20 17:51:59 -0700

    changed to openweathermap api

--- weather.js
+++ weather.js
@@ -1,6 +1,6 @@
 class Weather {
   constructor(city, state) {
-    this.apiKey = '99dfe35fcb7de1ee';
+    this.apiKey = 'c163266aa23691a29fdc499f18a534f6';
     this.city = city;
     this.state = state;
   }
@@ -8,8 +8,9 @@ class Weather {
   //fetch weather from api
   async getWeather() {
     const response = await fetch
-    (`http://api.wunderground.com/api/${this.apiKey}/conditions/q/${this.state}/${this.city}.json`);
+    (`http://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.state}&appid=${this.apiKey}`);
 
+    
     const responseData = await response.json();
 
     return responseData.current_observation;
