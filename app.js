b927a4f398c5ceedfca3da333057a58bd457daf3
carolgreene <carolgreene3@gmail.com>
2020-05-21 14:30:48 -0700

    changed state to country for new api site

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //Init weather object
-const weather = new Weather('Boston', 'MA');
+const weather = new Weather('chicago','US');
 
 //Get Weather on DOM load
 document.addEventListener('DOMContentLoaded', getWeather)
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', getWeather)
 function getWeather(){
   weather.getWeather() //will return a promise bc getWeather() is an async function
     .then(results => {
-      console.log(results)
+      console.log('results:', results)
     })
     .catch(err => console.log("something went wrong", err))
   }
--- weather.js
+++ weather.js
@@ -1,19 +1,21 @@
 class Weather {
-  constructor(city, state) {
-    this.apiKey = 'c163266aa23691a29fdc499f18a534f6';
+  constructor(city, country) {
+    this.apiKey = 'c163266aa23691a29fdc499f18a534f6'
     this.city = city;
-    this.state = state;
+    //this.state = 'NY';
+    this.country = country;
   }
 
   //fetch weather from api
   async getWeather() {
     const response = await fetch
-    (`http://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.state}&appid=${this.apiKey}`);
+    (`https://api.openweathermap.org/data/2.5/weather?q=${this.city},$
+    {this.country}&APPID=${this.apiKey}&units=imperial`);
 
     
     const responseData = await response.json();
 
-    return responseData.current_observation;
+    return responseData;
   }
 
   //Change weather location
