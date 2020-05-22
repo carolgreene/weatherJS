0ff92c65d8ebd73ef7f5dd4fed1bc029801d9bf2
carolgreene <carolgreene3@gmail.com>
2020-05-21 17:49:28 -0700

    working ui for initial fetch

---
+++ ui.js
@@ -0,0 +1,25 @@
+class UI {
+  constructor() {
+    this.location = document.getElementById('w-location');
+    this.desc = document.getElementById('w-desc')
+    this.string = document.getElementById('w-string')
+    this.details = document.getElementById('w-details')
+    this.icon = document.getElementById('w-icon')
+    this.humidity = document.getElementById('w-humidity')
+    this.feelsLike = document.getElementById('w-feels-like')
+    this.pressure = document.getElementById('w-pressure')
+    this.wind = document.getElementById('w-wind')
+  }
+
+  //need to adjust this for the new api I'm using to get the correct names
+  paint(weather) {
+    this.location.textContent = weather.name;
+    this.desc.textContent = weather.weather[0].description;
+    this.string.textContent = (Math.round(weather.main.temp) + ' F');
+    this.icon.setAttribute('src', `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`);
+    this.humidity.textContent = `Relative Humidity: ${weather.main.humidity}`;
+    this.feelsLike.textContent = `Feels Like: ${weather.main.feels_like}`;
+    this.pressure.textContent = `Pressure: ${weather.main.pressure}`;
+    this.wind.textContent = `Wind: ${weather.wind.speed}`;
+  }
+}
