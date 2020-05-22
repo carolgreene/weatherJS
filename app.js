3c46271eb36307bf21b91ac0f946c15d637ab48c
carolgreene <carolgreene3@gmail.com>
2020-05-21 17:50:37 -0700

    initialized ui & called paint method

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //Init weather object
 const weather = new Weather('chicago','US');
+const ui = new UI()
 
 //Get Weather on DOM load
 document.addEventListener('DOMContentLoaded', getWeather)
@@ -7,7 +8,8 @@ document.addEventListener('DOMContentLoaded', getWeather)
 function getWeather(){
   weather.getWeather() //will return a promise bc getWeather() is an async function
     .then(results => {
-      console.log('results:', results)
+      console.log(results)
+      ui.paint(results)
     })
     .catch(err => console.log("something went wrong", err))
   }
